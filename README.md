2943a5a48d4281b31fddd477f6f43516faddff51
NLSUSR <id9397512018@gmail.com>
2023-05-12 10:23:56 +0400

    gh-pages

--- README.md
+++
@@ -1,8 +0,0 @@
-# Используемые технологии :
-
-1. Node.js
-2. NPM
-3. React(create-react-app): HTML5, JS(ES22): .jsx
-4. Намек на "Tailwind" из библиотеки Яндекса , Модульный Sass(CSS3)
-
-Ознакомиться с макетом можно по ссылке: https://bit.ly/3IZ7rSr
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app/app.jsx";
-import reportWebVitals from "./reportWebVitals.ts";
-
-ReactDOM.createRoot(document.querySelector("#root")).render(<React.StrictMode children={<App />} />);
-
-reportWebVitals();
